58d8cbde39298489f12ec69b36d885495f83366e
OkaRitsu <riku26clm@gmail.com>
2021-10-15 14:33:51 +0900

    コンタクトフォームの外観作成

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.views.generic.base import View
 
 from . import views
 
 
 app_name = 'products'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,10 @@
 from django.views import generic
 
+from .forms import ContactForm
+
 class IndexView(generic.TemplateView):
     template_name = 'index.html'
+
+class ContactView(generic.FormView):
+    template_name = 'contact.html'
+    form_class = ContactForm
